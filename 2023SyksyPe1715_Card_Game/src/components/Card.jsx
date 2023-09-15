ba8911beab7991b09b52f754e200e5a38053d749
DESKTOP-TEM8N3F\Mutti8888 <taavi.makela@gmail.com>
2023-09-15 18:14:06 +0300

    props aloitettu ja css tehty

--- 2023SyksyPe1715_Card_Game/src/components/Card.jsx
+++ 2023SyksyPe1715_Card_Game/src/components/Card.jsx
@@ -1,6 +1,15 @@
 
 export default function Card(){
     return(
-        <img src="http://placekitten.com/120/100"/>
+        <div className="card">
+            <img src="http://placekitten.com/120/100"/>
+            <ul className="stat-list">
+                <li className="stat-list-item">
+                    <span>Cuteness</span>
+                    <span>10</span>
+                </li>
+            </ul>
+        </div>
+
     );
 }

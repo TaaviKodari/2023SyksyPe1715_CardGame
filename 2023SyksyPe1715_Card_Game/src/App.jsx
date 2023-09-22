baf4dfff32b9297bf4c032216191f012cbf2c7c8
DESKTOP-TEM8N3F\Mutti8888 <taavi.makela@gmail.com>
2023-09-22 18:12:31 +0300

    map ja vastustajan kortti

--- 2023SyksyPe1715_Card_Game/src/App.jsx
+++ 2023SyksyPe1715_Card_Game/src/App.jsx
@@ -3,15 +3,23 @@ import './App.css'
 
 
 const playerCard ={
-  image: 'http://placekitten.com/120/100',
-  stats:[{name: 'Cuteness', value: 9}]
+  image: 'http://placekitten.com/120/100?image=0',
+  stats:[{name: 'Cuteness', value: 90},
+         {name: 'Speed', value: 5}]
+};
+
+const opponentCard ={
+  image: 'http://placekitten.com/120/100?image=2',
+  stats:[{name: 'Cuteness', value: 100},
+         {name: 'Speed', value: 25}]
 };
 
 export default function App(){
   return(
     <>
       <h1>Hello world!</h1>
-      <Card />
+      <Card card = {playerCard}/>
+      <Card card = {opponentCard}/>
     </>
   );
 }

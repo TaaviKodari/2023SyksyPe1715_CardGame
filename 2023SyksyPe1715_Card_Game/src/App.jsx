e88de1c1969dc9f210f91e81df8f9597b3bad147
DESKTOP-TEM8N3F\Mutti8888 <taavi.makela@gmail.com>
2023-09-29 18:14:08 +0300

    comparecards tehty

--- 2023SyksyPe1715_Card_Game/src/App.jsx
+++ 2023SyksyPe1715_Card_Game/src/App.jsx
@@ -4,22 +4,45 @@ import './App.css'
 
 const playerCard ={
   image: 'http://placekitten.com/120/100?image=0',
-  stats:[{name: 'Cuteness', value: 90},
+  stats:[{name: 'Cuteness', value: 100},
          {name: 'Speed', value: 5}]
 };
 
 const opponentCard ={
   image: 'http://placekitten.com/120/100?image=2',
-  stats:[{name: 'Cuteness', value: 100},
+  stats:[{name: 'Cuteness', value: 1},
          {name: 'Speed', value: 25}]
 };
 
 export default function App(){
+  
+  function compareCards(){
+    const playerStat = playerCard.stats[0];
+    const opponentStat = opponentCard.stats[0];
+
+    let result = '';
+
+    if(playerStat.value === opponentStat.value){
+      result = 'Draw';
+    }
+    else if(playerStat.value > opponentStat.value){
+      result = 'Win';
+    }
+    else{
+      result = "Loss";
+    }
+
+    console.log(result);
+  }
+  
   return(
     <>
       <h1>Hello world!</h1>
-      <Card card = {playerCard}/>
-      <Card card = {opponentCard}/>
+      <div className='game'>
+        <Card card = {playerCard}/>
+        <button onClick={compareCards} type="button">Play</button>
+        <Card card = {opponentCard}/> 
+      </div>
     </>
   );
 }
